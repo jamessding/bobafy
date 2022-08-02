7d823a0bd43646f93a84498af1f58405625572d8
James Ding <jamesding528@yahoo.com>
2022-08-02 00:00:20 +0000

    created info window when user clicks on a marker, changed map styling to be more minimal

--- client/components/map.jsx
+++ client/components/map.jsx
@@ -1,14 +1,54 @@
+/* global google */
 import React, { useState, useEffect } from 'react';
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import { GoogleMap, LoadScript, Marker, InfoWindow } from '@react-google-maps/api';
+import Rating from './rating';
 
 const containerStyle = {
   width: '100%',
   height: '100vh'
 };
 
+const styles = [
+  {
+    featureType: 'administrative',
+    elementType: 'geometry',
+    stylers: [
+      {
+        visibility: 'off'
+      }
+    ]
+  },
+  {
+    featureType: 'poi',
+    stylers: [
+      {
+        visibility: 'off'
+      }
+    ]
+  },
+  {
+    featureType: 'road',
+    elementType: 'labels.icon',
+    stylers: [
+      {
+        visibility: 'off'
+      }
+    ]
+  },
+  {
+    featureType: 'transit',
+    stylers: [
+      {
+        visibility: 'off'
+      }
+    ]
+  }
+];
+
 export default function Map(props) {
   const [currentPosition, setCurrentPosition] = useState({ lat: 33.6349, lng: -117.7405 });
   const [yelpResults, setYelpResults] = useState([]);
+  const [selected, setSelected] = useState({});
 
   const success = position => {
     const currentPosition = {
@@ -22,6 +62,10 @@ export default function Map(props) {
     const lat = e.latLng.lat();
     const lng = e.latLng.lng();
     setCurrentPosition({ lat, lng });
+  };
+
+  const onSelect = item => {
+    setSelected(item);
   };
 
   useEffect(() => {
@@ -56,6 +100,7 @@ export default function Map(props) {
         mapContainerStyle={containerStyle}
         center={currentPosition}
         zoom={13}
+        options={{ styles }}
       >
         {
           yelpResults.map(result => {
@@ -63,9 +108,31 @@ export default function Map(props) {
               <Marker
                 key={result.id}
                 position={{ lat: result.coordinates.latitude, lng: result.coordinates.longitude }}
+                onClick={() => onSelect(result)}
+                icon={{
+                  url: 'https://cdn-icons-png.flaticon.com/512/7301/7301959.png',
+                  scaledSize: new google.maps.Size(50, 50)
+                }}
+                animation={google.maps.Animation.DROP}
               />
             );
           })
+        }
+        {
+          selected.location &&
+          (
+            <InfoWindow
+              position={{ lat: selected.coordinates.latitude, lng: selected.coordinates.longitude }}
+              clickable={true}
+              onCloseClick={() => setSelected({})}
+            >
+              <>
+                <p>{selected.name}</p>
+                <Rating rating={selected.rating} />
+                <img className='info-image' src={selected.image_url}></img>
+              </>
+            </InfoWindow>
+          )
         }
         <Marker
           position={currentPosition}
