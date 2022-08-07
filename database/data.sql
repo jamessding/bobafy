2e5b8f824554deb24b37970aafd29d9b5132bfa2
James Ding <jamesding528@yahoo.com>
2022-08-07 07:49:10 +0000

    created database with test data for user

---
+++ database/data.sql
@@ -0,0 +1,2 @@
+insert into "users" ("userId", "username", "hashedPassword", "firstName", "lastName", "avatarUrl")
+values (1, 'anonymous', '$argon2i$v=19$m=4096,t=3,p=1$h7icQD/xZr8akZsX+hNA0A$h68atJWyjvunAwNOpSpMfg9sPvoMQ6dKwoh0dJhurWA', 'James', 'Ding', 'https://www.pngitem.com/pimgs/m/30-307416_profile-icon-png-image-free-download-searchpng-employee.png');
